5bb9789afe981eb459fdfb1465d1852e59acc485
Sanskruti Itkar <139566431+Sanskrutiitkar@users.noreply.github.com>
2024-07-17 22:17:36 +0530

    Add files via upload

---
+++ ProductApp/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ProductApp.Product;
+
+namespace ProductApp
+{
+    internal class Product
+    {
+        public class Products 
+        {
+            private int productId;
+            private string productname;
+            private int productPrice;
+            private int productDiscountPercentage;
+
+        public Products(int productId, string productname, int productPrice, int productDiscountPercentage)
+        {
+                this.productId = productId;
+                this.productPrice = productPrice;
+                this.productname = productname;
+                this.productDiscountPercentage = productDiscountPercentage;
+        }
+        public double productDiscountedPrice()
+            {
+                double productDiscountedPrice= productPrice - (productPrice * productDiscountPercentage / 100);
+                return productDiscountedPrice;
+            }
+        }
+    }
+}
---
+++ ProductApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ProductApp.Product;
+
+namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Products product1 = new Products(10,"biscuit",2000,10);
+            Console.WriteLine("Discounted price is  = "+ product1.productDiscountedPrice());
+            
+        }
+    }
+}
